9624809d36cd8babd88a723cf50be9f96cf4a464
Commander07 <45269106+Commander07@users.noreply.github.com>
2023-04-02 14:36:07 +0200

    Loader support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use semver::{BuildMetadata, Prerelease, Version, VersionReq};
 use serde::{Deserialize, Serialize};
-use std::{fs, path::Path};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
 
 const CURRENT_MANIFEST_VERSION: Version = Version {
     major: 0,
@@ -11,7 +14,7 @@ const CURRENT_MANIFEST_VERSION: Version = Version {
 };
 
 trait Downloadable {
-    fn download(&self, modpack_root: &str);
+    fn download(&self, modpack_root: &PathBuf);
 }
 #[derive(Debug, Deserialize, Serialize)]
 struct Mod {
@@ -40,17 +43,72 @@ struct Loader {
     version: String,
     minecraft_version: String,
 }
-
+impl Downloadable for Loader {
+    fn download(&self, modpack_root: &PathBuf) {
+        match self.r#type.as_str() {
+            "fabric" => download_fabric(&self, modpack_root),
+            _ => panic!("Unsupported loader '{}'!", self.r#type.as_str()),
+        }
+    }
+}
 #[derive(Debug, Deserialize, Serialize)]
 struct Manifest {
     manifest_version: String,
     modpack_version: String,
     name: String,
     uuid: String,
+    loader: Loader,
     mods: Vec<Mod>,
     shaderpacks: Vec<Shaderpack>,
     resourcepacks: Vec<Resourcepack>,
     include: Vec<String>,
+}
+
+fn download_fabric(loader: &Loader, modpack_root: &PathBuf) {
+    let url = format!(
+        "https://meta.fabricmc.net/v2/versions/loader/{}/{}/profile/json",
+        loader.minecraft_version, loader.version
+    );
+    let loader_name = format!(
+        "fabric-loader-{}-{}",
+        &loader.version, &loader.minecraft_version
+    );
+    let resp = reqwest::blocking::get(url.as_str())
+        .expect("Failed to download fabric loader!")
+        .text()
+        .unwrap();
+    let fabric_path = modpack_root.join(&Path::new(&format!("versions/{}", &loader_name)));
+    fs::create_dir_all(&fabric_path).expect("Failed to create fabric directory");
+    fs::write(
+        &fabric_path.join(Path::new(&format!("{}.json", &loader_name))),
+        resp,
+    )
+    .expect("Failed to write fabric json");
+    fs::write(
+        &fabric_path.join(Path::new(&format!("{}.jar", &loader_name))),
+        "",
+    )
+    .expect("Failed to write fabric dummy jar");
+}
+
+fn get_minecraft_folder() -> PathBuf {
+    if env::consts::OS == "linux" {
+        Path::new(&(dirs::home_dir().unwrap().to_str().unwrap().to_owned() + "/.minecraft"))
+            .to_owned()
+    } else if env::consts::OS == "windows" {
+        Path::new(&(dirs::config_dir().unwrap().to_str().unwrap().to_owned() + "\\.minecraft"))
+            .to_owned()
+    } else if env::consts::OS == "macos" {
+        Path::new(&(dirs::config_dir().unwrap().to_str().unwrap().to_owned() + "/minecraft"))
+            .to_owned()
+    } else {
+        panic!("Unsupported os '{}'!", env::consts::OS)
+    }
+}
+fn get_modpack_root(modpack_uuid: &str) -> PathBuf {
+    let root = get_minecraft_folder().join(Path::new(&format!(".WC_OVHL/{}", modpack_uuid)));
+    fs::create_dir_all(&root).expect("Failed to create modpack folder");
+    return root;
 }
 
 fn main() {
@@ -67,4 +125,7 @@ fn main() {
         "Unsupported manifest version '{}'!",
         manifest.manifest_version
     );
+
+    let modpack_root = get_modpack_root(&manifest.uuid);
+    manifest.loader.download(&modpack_root);
 }
