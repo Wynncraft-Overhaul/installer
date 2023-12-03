8cb5214a25f287a1e05d2707be2f79cf70fd61f1
Commander07 <45269106+Commander07@users.noreply.github.com>
2023-12-03 16:48:14 +0100

    [no ci]

--- scripts/build-appbundle.sh
+++ scripts/build-appbundle.sh
@@ -1,12 +1,11 @@
-export DESTINATION = macos_bundle
-mkdir $DESTINATION/
-mkdir $DESTINATION/Installer.app/
-mkdir $DESTINATION/Installer.app/Contents/
-mkdir $DESTINATION/Installer.app/Contents/Resources
-mkdir $DESTINATION/Installer.app/Contents/MacOS/
-cp icon_256.icns $DESTINATION/Installer.app/Contents/Resources/icon_256.icns
-cp target/debug/installer $DESTINATION/Installer.app/Contents/MacOS/installer
-cat > $DESTINATION/Installer.app/Contents/Info.plist << EOF
+mkdir macos_bundle/
+mkdir macos_bundle/Installer.app/
+mkdir macos_bundle/Installer.app/Contents/
+mkdir macos_bundle/Installer.app/Contents/Resources
+mkdir macos_bundle/Installer.app/Contents/MacOS/
+cp icon_256.icns macos_bundle/Installer.app/Contents/Resources/icon_256.icns
+cp target/debug/installer.exe macos_bundle/Installer.app/Contents/MacOS/installer
+cat > macos_bundle/Installer.app/Contents/Info.plist << EOF
 {
    CFBundleName = installer;
    CFBundleDisplayName = Majestic Overhaul Installer;
