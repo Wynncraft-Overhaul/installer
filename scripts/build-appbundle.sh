799d9ce2184442d218486c227ca0a9031b24d336
Commander07 <45269106+Commander07@users.noreply.github.com>
2023-12-03 16:52:16 +0100

    [no ci]

--- scripts/build-appbundle.sh
+++ scripts/build-appbundle.sh
@@ -4,7 +4,7 @@ mkdir macos_bundle/Installer.app/Contents/
 mkdir macos_bundle/Installer.app/Contents/Resources
 mkdir macos_bundle/Installer.app/Contents/MacOS/
 cp icon_256.icns macos_bundle/Installer.app/Contents/Resources/icon_256.icns
-cp target/debug/installer.exe macos_bundle/Installer.app/Contents/MacOS/installer
+cp target/debug/installer macos_bundle/Installer.app/Contents/MacOS/installer
 cat > macos_bundle/Installer.app/Contents/Info.plist << EOF
 {
    CFBundleName = installer;
