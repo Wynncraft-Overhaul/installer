57d075930dae94b0c1c30360f3f99d2c6c1631f4
Olinus10 <115377234+Olinus10@users.noreply.github.com>
2025-03-11 03:51:23 +0100

    Update gui.rs

--- src/gui.rs
+++ src/gui.rs
@@ -841,7 +841,7 @@ div {
             class: "install-button",
             disabled: install_disable,
             style: "width: 100%; padding: 10px; font-size: 1.2em;" // Ensure button is large enough and visible
- }
+             }
                         }
                     }
                 }
