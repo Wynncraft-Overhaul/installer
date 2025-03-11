df8ea677f1bca5af246b12bc716c6e39058348b2
Olinus10 <115377234+Olinus10@users.noreply.github.com>
2025-03-11 07:58:30 +0100

    Update gui.rs

--- src/gui.rs
+++ src/gui.rs
@@ -847,12 +847,15 @@ rsx! {
                                                     "{desc}",
                                                 }),
                                                 None => rsx!("")
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                    }
+ 
+                }
+ 
+            }
+ 
+        }
+ 
+    }
                         div {
                             style: "padding: 10px; display: flex; justify-content: center; align-items: center; background-color: #f8f8f8; transition: transform 0.2s ease-in-out;",
                             onmouseover: "this.style.transform='scale(1.05)'",
